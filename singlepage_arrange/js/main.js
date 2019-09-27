9478099380c27bc0264e5aa891562477b2557dc4
u-rr <fashiondrop@gmail.com>
2019-09-27 19:49:28 +0900

    ハンバーガー出し入れできた！

--- singlepage_arrange/js/main.js
+++ singlepage_arrange/js/main.js
@@ -1,14 +1,20 @@
 "use strict";
 
-const toggle_close = document.getElementById('fa-times');
-const toggle_open = document.getElementById('fa-bars');
-const toggle_menu = document.getElementById('toggle_menu');
+{
+  // ×ボタンを取得
+  const toggle_close = document.getElementById('fa-times');
+  // ハンバーガーボタンを取得
+  const toggle_open = document.getElementById('fa-bars');
+  // ハンバーガー押したときのメニューを取得
+  const toggle_menu = document.getElementById('toggle_menu');
 
-toggle_open.addEventListener('click', () => {
-  toggle_menu.classList.toggle('open')
-});
+  // ハンバーガーを押したときにopenクラスをつける
+  toggle_open.addEventListener('click', () => {
+    toggle_menu.classList.add('open')
+  });
 
-toggle_close.addEventListener('click', () => {
-  // alert('テスト');
-  toggle_menu.classList.toggle('close');
-});
+  // ×を押したときにopenクラスを外す
+  toggle_close.addEventListener('click', () => {
+    toggle_menu.classList.remove('open');
+  });
+}
